c7c3ba9dc691540d9ae7633bed1736348a2011a8
Helbak <helbak77@gmail.com>
2019-05-21 21:41:07 +0300

    CalculatorHTML + JS with tests

---
+++ Test.js
@@ -0,0 +1,115 @@
+describe(" test buttons", () => {
+    before(() => {
+
+    });
+    it(`test 1`, function () {
+        const act = writeOne();
+        const exp = '1';
+        assert.deepEqual(act, exp);
+    });
+        it(`test 12`, function () {
+        const act = writeTwo();
+        const exp = '12';
+        assert.deepEqual(act, exp);
+    });
+    it(`test 123`, function () {
+        const act = writeThree();
+        const exp = '123';
+        assert.deepEqual(act, exp);
+    });
+    it(`test 1234`, function () {
+        const act = writeFour();
+        const exp = '1234';
+        assert.deepEqual(act, exp);
+    });
+    it(`test 12345`, function () {
+        const act = writeFive();
+        const exp = '12345';
+        assert.deepEqual(act, exp);
+    });
+    it(`test 123456`, function () {
+        const act = writeSix();
+        const exp = '123456';
+        assert.deepEqual(act, exp);
+    });
+        it(`test 1234567`, function () {
+        const act = writeSeven();
+        const exp = '1234567';
+        assert.deepEqual(act, exp);
+    });
+    it(`test 12345678`, function () {
+        const act = writeEight();
+        const exp = '12345678';
+        assert.deepEqual(act, exp);
+    });
+    it(`test upgrade`, function () {
+        const act = allNull();
+        const exp = '';
+        assert.deepEqual(act, exp);
+    });
+    it(`test 123456789`, function () {
+        const act = writeNine();
+        const exp = '9';
+        assert.deepEqual(act, exp);
+    });
+    it(`test dot`, function () {
+        const act = writeDot();
+        const exp = '9.';
+        assert.deepEqual(act, exp);
+    });
+    it(`test 9.0`, function () {
+        const act = writeZero();
+        const exp = '9.0';
+        assert.deepEqual(act, exp);
+    });
+    it(`test  plus`, function () {
+        writeActionPlus();
+        writeThree();
+        const act = getResult();
+            const exp = 12;
+        assert.deepEqual(act, exp);
+    });
+    it(`test divide`, function () {
+        writeActionDivide();
+        writeTwo();
+        const act = getResult();
+        const exp = 6;
+        assert.deepEqual(act, exp);
+    });
+    it(`test multiplication`, function () {
+        writeActionMulti();
+        writeTwo();
+        const act = getResult();
+        const exp = 12;
+        assert.deepEqual(act, exp);
+    });
+    it(`test minus`, function () {
+        writeActionMinus();
+        writeFour();
+        const act = getResult();
+        const exp = 8;
+        assert.deepEqual(act, exp);
+    });
+    it(`test max signs`, function () {
+       allNull();
+        writeFour();
+        writeFour();
+        writeFour();
+        writeFour();
+        writeFour();
+        writeFour();
+        writeFour();
+        writeFour();
+        writeFour();
+        writeFour();
+        const act =  writeFour();
+        const exp = '44444444';
+        assert.deepEqual(act, exp);
+    });
+    it(`test upgrade`, function () {
+        const act = allNull();
+        const exp = '';
+        assert.deepEqual(act, exp);
+    });
+
+});
---
+++ init.js
@@ -0,0 +1,193 @@
+// function init() {
+    let num1 = '';
+    let oldNum;
+    let result = 0;
+    let num = '';
+    let action;
+
+    const inputFirst = document.getElementById('inputFirst');
+    const one = document.getElementById('one');
+    const two = document.getElementById('two');
+    const three = document.getElementById('three');
+    const four = document.getElementById('four');
+    const plus = document.getElementById('plus');
+    const minus = document.getElementById('minus');
+    const divide = document.getElementById('divide');
+    const multi = document.getElementById('multi');
+    const five = document.getElementById('five');
+    const six = document.getElementById('six');
+    const seven = document.getElementById('seven');
+    const eight = document.getElementById('eight');
+    const nine = document.getElementById('nine');
+    const zero = document.getElementById('zero');
+    const dot = document.getElementById('dot');
+    const reset = document.getElementById('reset');
+    const equal = document.getElementById('equal');
+
+    reset.addEventListener('click', allNull, true);
+    one.addEventListener('click', writeOne, true);
+    two.addEventListener('click', writeTwo, true);
+    three.addEventListener('click', writeThree, true);
+    four.addEventListener('click', writeFour, true);
+    five.addEventListener('click', writeFive, true);
+    six.addEventListener('click', writeSix, true);
+    seven.addEventListener('click', writeSeven, true);
+    eight.addEventListener('click', writeEight, true);
+    nine.addEventListener('click', writeNine, true);
+    zero.addEventListener('click', writeZero, true);
+    dot.addEventListener('click', writeDot, true);
+    plus.addEventListener('click', writeActionPlus, true);
+    equal.addEventListener('click', getResult, true);
+    minus.addEventListener('click', writeActionMinus, true);
+    divide.addEventListener('click', writeActionDivide, true);
+    multi.addEventListener('click', writeActionMulti, true);
+
+    function writeOne() {
+        if (num1.length <= 7)
+            num1 = num1 + '1';
+        toScreen(num1);
+        return num1;
+    };
+
+    function writeTwo() {
+        if (num1.length <= 7)
+            num1 = num1 + '2';
+        toScreen(num1);
+        return num1;
+    };
+
+    function writeThree() {
+        if (num1.length <= 7)
+            num1 = num1 + '3';
+        toScreen(num1);
+        return num1;
+    };
+
+    function writeFour() {
+        if (num1.length <= 7)
+            num1 = num1 + '4';
+        toScreen(num1);
+        return num1;
+    };
+
+    function writeFive() {
+        if (num1.length <= 7)
+            num1 = num1 + '5';
+        toScreen(num1);
+        return num1;
+    };
+
+    function writeSix() {
+        if (num1.length <= 7)
+            num1 = num1 + '6';
+        toScreen(num1);
+        return num1;
+    };
+
+    function writeSeven() {
+        if (num1.length <= 7)
+            num1 = num1 + '7';
+        toScreen(num1);
+        return num1;
+    };
+
+    function writeEight() {
+        if (num1.length <= 7)
+            num1 = num1 + '8';
+        toScreen(num1);
+        return num1;
+    };
+
+    function writeNine() {
+        if (num1.length <= 7)
+            num1 = num1 + '9';
+        toScreen(num1);
+        return num1;
+    };
+
+    function writeZero() {
+        if (num1.length <= 7)
+            num1 = num1 + '0';
+        toScreen(num1);
+        return num1;
+    };
+
+    function writeDot() {
+        if (num1.length <= 6)
+            num1 = num1 + '.';
+        toScreen(num1);
+        dot.disabled = true;
+        return num1;
+    };
+
+    function saveOldNum() {
+        dot.disabled = false;
+        oldNum = parseInt(num1);
+        num1 = '';
+    };
+
+    function writeActionPlus() {
+        saveOldNum();
+        toScreen('');
+        action = doPlus;
+    };
+
+    function writeActionMinus() {
+        saveOldNum();
+        toScreen('');
+        action = doMinus;
+    };
+
+    function writeActionDivide() {
+        saveOldNum();
+        toScreen('');
+        action = doDivide;
+    };
+
+    function writeActionMulti() {
+        saveOldNum();
+        toScreen('');
+        action = doMulti;
+    };
+
+    function doPlus() {
+        console.log('doPlus    ' + oldNum);
+        return oldNum + parseInt(num1);
+    };
+
+    function doMinus() {
+        return oldNum - parseInt(num1);
+    };
+
+    function doDivide() {
+        return oldNum / parseInt(num1);
+    };
+
+    function doMulti() {
+        return oldNum * parseInt(num1);
+    };
+
+    function getResult() {
+        result = action(oldNum, num1);
+        num1 = result;
+        toScreen(num1);
+        return num1;
+    };
+
+    function allNull() {
+        dot.disabled = false;
+        oldNum = 0;
+        num1 = '';
+        toScreen(0);
+        return num1;
+    };
+
+    function toScreen(n) {
+        inputFirst.value = n;
+    };
+    // inputFirst.value = num;
+// }
+//
+// init();
+
+
